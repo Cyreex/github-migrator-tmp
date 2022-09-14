0d9a13ab1f0ced28fcd8e0d0f1cec7e5d8e38485
Artem Romanchik <a.romanchik@gmail.com>
2022-09-14 09:45:38 +0300

    Update migrate_gitlab_repos.sh

--- migrate_gitlab_repos.sh
+++ migrate_gitlab_repos.sh
@@ -1,3 +1,4 @@
+debug_level=0
 old_gitlab="gitlab.tpondemand.net"
 new_gitlab="awsgitlab.tpondemand.net"
 
@@ -5,13 +6,13 @@ working_dir=$(pwd)
 git_repos=$(find ./ -name "*.git" -type d)
 
 for git_repo in ${git_repos}; do
-  echo "Change folder to ${git_repo}"
+  [ $debug_level -eq 1 ] && echo "Change folder to ${git_repo}"
   cd ${git_repo}/../
   is_old_gitlab=$(git remote get-url --push origin | grep ^git@${old_gitlab} -c || true)
 
   if [ ${is_old_gitlab} -eq 1 ]; then
     git_repo_path=$(cut -d: -f2 <<< ${git_repo})
-    echo "Check the repo ${git_repo_path} exists in ${new_gitlab}"
+    [ $debug_level -eq 1 ] && echo "Check the repo ${git_repo_path} exists in ${new_gitlab}"
 
     git ls-remote git@${new_gitlab}:${git_repo_path} &>/dev/null
     exit_status=$?
@@ -20,7 +21,7 @@ for git_repo in ${git_repos}; do
       echo "The repo ${git_repo_path} exists in ${new_gitlab}. Set a new remote url for the repo ${git_repo}"
       git remote set-url origin git@${new_gitlab}:${git_repo_path}
     else
-      echo "The repo ${git_repo_path} doesn't exists in ${new_gitlab}. No changes needed."
+      [ $debug_level -eq 1 ] && echo "The repo ${git_repo_path} doesn't exists in ${new_gitlab}. No changes needed."
     fi
   fi
   cd ${working_dir}
